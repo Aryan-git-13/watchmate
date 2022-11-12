afedf0222ec5b02ab232b10fc35a9881d8d46c17
Aroo13 <aroonba13thknight@gmail.com>
2022-11-12 22:03:54 +0530

    complete API with Django

--- watchlist_app/urls.py
+++ watchlist_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
-from watchlist_app.views import movie_list
+from watchlist_app.views import movie_detail, movie_list
 urlpatterns = [
     path('list/', movie_list, name='movie-list'),
+    path('<int:pk>', movie_detail, name='movie_details')
 ]
--- watchlist_app/views.py
+++ watchlist_app/views.py
@@ -6,3 +6,12 @@ def movie_list(request):
     movies = Movie.objects.all()
     return JsonResponse({'movies':list(movies.values())})
 
+def movie_detail(request, pk):
+    movie = Movie.objects.get(pk=pk) 
+    print(movie)
+    data = {
+        'name': movie.name,
+        'description': movie.description,
+        'active': movie.active
+    }
+    return JsonResponse(data)
